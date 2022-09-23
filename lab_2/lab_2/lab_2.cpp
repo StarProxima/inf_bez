4d505ecb12b3396501e93f0724f5019e35e7ecff
StarProxima <maks.prozorov1@yandex.ru>
2022-09-24 01:29:54 +0300

    Brute-force solution

--- lab_2/lab_2/lab_2.cpp
+++ lab_2/lab_2/lab_2.cpp
@@ -1,20 +1,105 @@
-﻿// lab_2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿#include <string>
+#include <vector>
+#include <fstream>
 #include <iostream>
+#include <iomanip>
+#include <algorithm>
+using namespace std;
 
-int main()
+
+void swap(char* a, int i, int j)
 {
-    std::cout << "Hello World!\n";
+	int s = a[i];
+	a[i] = a[j];
+	a[j] = s;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+bool nextP(char* a, int n)
+{
+	int j = n - 2;
+	while (j != -1 && a[j] >= a[j + 1]) j--;
+	if (j == -1)
+		return false;
+	int k = n - 1;
+	while (a[j] >= a[k]) k--;
+	swap(a, j, k);
+	int l = j + 1, r = n - 1;
+	while (l < r)
+		swap(a, l++, r--);
+	return true;
+}
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+int main()
+{	
+	int number;
+	cin >> number;
+
+
+	char perm[] = "ABCDEFGHIJKLMNOPQRSTUVWXY";
+
+	int curNumber = 0;
+	char word[5][5];
+	bool trueWord = true;
+
+	string ans = "";
+	int i = 0;
+
+
+	do {
+		
+
+		trueWord = true;
+
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				word[i][j] = perm[i * 5 + j];
+			}
+		}
+
+		for (int i = 0; i < 5 && trueWord; i++) {
+			for (int j = 0; j < 4; j++) {
+				//std::cout << (int)word[i][j] << " >= " << (int)word[i][j + 1] << "|";
+
+				if ((int) word[i][j] >= (int)word[i][j + 1]) {
+					trueWord = false;
+					break;
+				}
+			}
+			//std::cout << endl;
+		}
+
+		//std::cout << trueWord;
+
+		for (int i = 0; i < 5 && trueWord; i++) {
+			for (int j = 0; j < 4; j++) {
+
+				if ((int)word[j][i] >= (int)word[j + 1][i]) {
+					trueWord = false;
+					break;
+				}
+			}
+		}
+
+		//std::cout << perm << trueWord << endl;
+		
+		if (trueWord) {
+			curNumber++;
+			if (curNumber % 5 == 1) {
+				for (int i = 0; i < 5; i++) {
+					std::cout << perm[i * 5] << perm[i * 5 + 1] << perm[i * 5 + 2] << perm[i * 5 + 3] << perm[i * 5 + 4] << " ";
+				}
+				std::cout << curNumber << endl;
+			}
+	
+			if (curNumber == number) {
+				ans = perm;
+				break;
+			}
+		}
+		i++;
+	} while (nextP(perm, 25) );
+
+
+	std::cout << ans << ", iters: " << i << endl;
+}
+
