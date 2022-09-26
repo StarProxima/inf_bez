93c9162296a3e399c34127990497abea71b24ba3
StarProxima <maks.prozorov1@yandex.ru>
2022-09-26 20:21:56 +0300

    Int to enum

--- lab_4/lab_4/lab_4.cpp
+++ lab_4/lab_4/lab_4.cpp
@@ -6,29 +6,34 @@
 #include <algorithm>
 using namespace std;
 
-int** getGrid( int& r, int& c, int& m, int& n) {
+enum gridValue {space, slash, backslash};
+
+enum direction {top, right, bottom, left};
+
+
+gridValue** getGrid(int& r, int& c, int& m, int& n) {
 	
 	ifstream inFile = ifstream("safe.in");
 
 	inFile >> r >> c >> m >> n;
 
-	int** grid;
-	grid = new int* [r];
+	gridValue** grid;
+	grid = new gridValue * [r];
 
 	for (int i = 0; i < r; i++) {
-		grid[i] = new int[c] {0};
+		grid[i] = new gridValue[c] {space};
 	}
 
 	int r1, r2;
 
 	for (int i = 0; i < m; i++) {
 		inFile >> r1 >> r2;
-		grid[r1 - 1][r2 - 1] = 1;
+		grid[r1 - 1][r2 - 1] = slash;
 	}
 
 	for (int i = 0; i < n; i++) {
 		inFile >> r1 >> r2;
-		grid[r1 - 1][r2 - 1] = 2;
+		grid[r1 - 1][r2 - 1] = backslash;
 	}
 
 	inFile.close();
@@ -43,7 +48,7 @@ int main()
 {
 	int r, c, m, n;
 
-	int** grid = getGrid(r, c, m, n);
+	gridValue** grid = getGrid(r, c, m, n);
 
 	for (int i = 0; i < r; i++) {
 		for (int j = 0; j < c; j++) {
@@ -51,4 +56,10 @@ int main()
 		}
 		cout << endl;
 	}
+
+
+
+
+
+
 }
